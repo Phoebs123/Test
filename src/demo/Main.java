553ac94d4c92b62b2ea324b8178f101f1cd32d89
Phoebs123 <phoe0142@stud.kea.dk>
2022-02-25 09:19:48 +0100

    initial commit

---
+++ src/demo/Main.java
@@ -0,0 +1,12 @@
+package demo;
+
+public class Main {
+
+    void go() {
+        System.out.println("It works");
+    }
+
+    public static void main(String[] args) {
+       new Main().go();
+    }
+}
