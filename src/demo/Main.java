1edd34f0c5fefeaeeac7d8bbbfe8ab613c276e82
Phoebs123 <phoe0142@stud.kea.dk>
2022-02-25 09:37:00 +0100

    added askAboutAge

--- src/demo/Main.java
+++ src/demo/Main.java
@@ -4,18 +4,27 @@ import java.util.Scanner;
 public class Main {
     Scanner in = new Scanner(System.in);
     String name;
+    int age;
+
+    int askAboutAge(int age) {
+        System.out.print("How old are you? ");
+        age = in.nextInt();
+        return age;
+    }
 
     void greet() {
         System.out.print("What is your name: ");
         name = in.nextLine();
         System.out.println("Hi " + name);
-
-
     }
 
     void go() {
         greet();
+        int yourAge;
+        yourAge = age;
+        System.out.println("You are " + yourAge + " old");
         System.out.println("It works");
+
     }
 
     public static void main(String[] args) {
